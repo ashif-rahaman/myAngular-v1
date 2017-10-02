bb06803c01e9980aeaa4bb9ec05a8618920a02ec
ashif-rahaman <ashifrahamanuis@gmail.com>
2017-10-03 02:43:42 +0600

    Part 2

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,46 +1,9 @@
-var main = angular.module("main", []);
+var module1 = angular.module('myApplication', []);
 
-//var image = {
-//
-//    url: "images/hill.jpg"
-//};
-//
-//var employees = [
-//
-//    {name: "Go"}
-//];
-//
-//var addButton = function (btnName) {
-//
-//    employees.assign({"name": btnName});
-//};
-//
-//var SimpleController = function ($scope) {
-//
-//    $scope.message = '';
-//    $scope.addButton = addButton();
-//    $scope.employees = employees;
-//
-//};
-//
-//main.controller("SimpleController", SimpleController);
+var controller1 = function ($scope) {
 
-main.controller('buttonAction', ['$scope', function ButtonAction(scope) {
-        scope.buttonName = 'Nothing';
-        scope.isActive = false;
-        scope.activate = function () {
-            scope.isActive = !scope.isActive;
-        };
-    }]);
+    $scope.message = 'Hello Bangladesh to Angular!!';
+};
 
-main.controller('addingNamesController', ['$scope', function AddingNamesController(scope) {
 
-        scope.users = [{name: 'Abc', age: 25}];
-        scope.name = '';
-        scope.age = 0;
-        scope.addName = function () {
-
-            var user = {name: scope.name, age: scope.age};
-            scope.users.push(user);
-        };
-    }]);
+module1.controller('myController', controller1);
