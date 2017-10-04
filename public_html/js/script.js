09ee14b8820e7da3bcae08dd95e8898c09fe71b6
ashif-rahaman <ashifrahamanuis@gmail.com>
2017-10-05 00:05:50 +0600

    Part 6: ng-repeat

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -3,14 +3,24 @@ var module1 = angular.module('myApplication', []);
 
 var controller1 = function ($scope) {
 
-    $scope.name = 'Hamim';
+    var countries = [
+
+        {
+            name: 'Bangladesh',
+            cities: ['Dhaka', 'Chittagong', 'Khulna City', 'Rajshahi']
+        },
+        {
+            name: 'India',
+            cities: ['Kalkatta', 'Delhi', 'Bombay']
+        },
+        {
+            name: 'Pakistan',
+            cities: ['Islamabad', 'Shindhu']
+        }
+    ];
+
+    $scope.countries = countries;
 };
 
-var controller2 = function ($scope) {
 
-    $scope.name = '';
-};
-
-
-module1.controller('myController1', controller1);
-module1.controller('myController2', controller2);
+module1.controller('myController', controller1);
